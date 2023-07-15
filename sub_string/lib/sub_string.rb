ef41e1bcfa6149c16ca93682857e95bb8fe81604
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-07-14 19:51:33 -0700

    Got substring to work with one word

--- sub_string/lib/sub_string.rb
+++ sub_string/lib/sub_string.rb
@@ -1,5 +1,22 @@
 class Sub_string
-    def substring
-
+    def substring (words, dictionary)
+        overlap = {}
+        dictionary.each do |word|
+            if (word == words)
+                if !overlap.key?(:"#{word}")
+                    overlap[:"#{word}"] = 1
+                else
+                    temp = overlap[:"#{word}"]
+                    overlap[:"#{word}"] = temp + 1
+                end
+            end
+        end
+        overlap
     end
 end
+
+dict = ["below","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+word = "below"
+
+thingy = Sub_string.new
+puts thingy.substring(word, dict)
