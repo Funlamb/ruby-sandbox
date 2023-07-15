361b766a78e834a234ace99dff0428d834c61f50
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-07-15 16:26:56 -0700

    Finished sub string project

--- sub_string/lib/sub_string.rb
+++ sub_string/lib/sub_string.rb
@@ -1,13 +1,16 @@
 class Sub_string
     def substring (words, dictionary)
         overlap = {}
+        words_split = words.downcase.gsub(/[^a-z0-9\s]/i, '').split(" ")
         dictionary.each do |word|
-            if (word == words)
-                if !overlap.key?(:"#{word}")
-                    overlap[:"#{word}"] = 1
-                else
-                    temp = overlap[:"#{word}"]
-                    overlap[:"#{word}"] = temp + 1
+            words_split.each do |check_word|
+                if check_word.include?word
+                    if !overlap.key?(:"#{word}")
+                        overlap[:"#{word}"] = 1
+                    else
+                        temp = overlap[:"#{word}"]
+                        overlap[:"#{word}"] = temp + 1
+                    end
                 end
             end
         end
@@ -16,7 +19,9 @@ class Sub_string
 end
 
 dict = ["below","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-word = "below"
+word = "belo"
 
 thingy = Sub_string.new
 puts thingy.substring(word, dict)
+many_words = "Howdy partner, sit down! How's it going?"
+puts thingy.substring(many_words, dict)
