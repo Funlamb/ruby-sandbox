fe30df8421096a7d78667adb9b8570ba4767043a
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-07-18 18:35:16 -0700

    Got board to output with to_s

--- tic_tac_toe/board.rb
+++ tic_tac_toe/board.rb
@@ -1,3 +1,18 @@
 # init the board
 # check for win
 # place a piece
+class Board
+    def initialize
+        @game_board = Array.new(9) {|i| (i+1)}
+    end
+    
+    def to_s
+        s = @game_board[0].to_s +  " | " + @game_board[1].to_s +  " | " + @game_board[2].to_s + "\n" +
+            "--+---+--\n" + 
+            @game_board[3].to_s +  " | " + @game_board[4].to_s +  " | " + @game_board[5].to_s + "\n" +
+            "--+---+--\n" + 
+            @game_board[6].to_s +  " | " + @game_board[7].to_s +  " | " + @game_board[8].to_s + "\n"; 
+        return s;
+    end
+    # puts @game_board
+end
--- tic_tac_toe/main.rb
+++ tic_tac_toe/main.rb
@@ -1,4 +1,6 @@
-require_relative "Player.rb"
+require_relative "player.rb"
+require_relative "board.rb"
+
 # make tic tac toe
 # use the command line
 # make two players
@@ -7,3 +9,6 @@ require_relative "Player.rb"
 
 player1 = Player.new("Mike", "O")
 puts player1
+
+game_board = Board.new()
+print game_board
