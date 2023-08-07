951de251db578795f4aad97d9463e588edf373ec
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-08-06 20:08:46 -0700

    Playing with IO

--- fileIO/read_write.rb
+++ fileIO/read_write.rb
@@ -2,6 +2,7 @@
 file = File.open("users.txt")
 file_data = file.read
 print file_data
+puts ''
 file.close
 
 # Write to a file
@@ -14,3 +15,17 @@ file = File.open("write.txt", "a") # a for append to a file
 file.write("#{Time.now} - user logged in.\n") 
 file.close
 
+# Read the position of the file
+file_data = []
+File.foreach("users.txt") do |line| 
+    puts line
+    file_data.push(line)
+end
+
+# Writing to a file moves the cursor. To read from that file again you must go back to the top
+fd = File.open("cursor.txt", "w")
+fd.puts 'Hello World'
+fd.puts 'goodbye world'
+puts fd.gets
+puts fd.pos
+puts fd.eof?
