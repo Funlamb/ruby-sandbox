a06de33aafb8942084ca69f33cec86a83e922107
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-08-13 14:13:11 -0700

    Started Hangman Project

---
+++ hangman/main.rb
@@ -0,0 +1,15 @@
+# Download the google words
+# Find words that are 5 to 12 letters long
+# Save new lest to a file so we don't have to do that again
+# Check if that file is available and use it
+
+# load the words
+
+# player gets 10 wrong guesses
+# show that player the word with blanks
+# show the player correct letters
+# show player wrong letters used
+# clear cmd every input so game stays up top
+
+# win condition
+# lose condition
