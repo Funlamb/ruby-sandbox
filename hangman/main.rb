c03b42777108ec46b1cf34cdf68cc8fc075e4d14
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-08-13 15:01:37 -0700

    Filtered the original google file

--- hangman/main.rb
+++ hangman/main.rb
@@ -1,5 +1,21 @@
 # Download the google words
 # Find words that are 5 to 12 letters long
+if (!File.exist?('google-10000-words-between-5-12.txt'))
+    filtered_words = []
+    File.foreach('google-10000-words.txt', chomp: true) do |line|
+        if (line.length.between?(5,12))
+            filtered_words.push line
+        end
+    end
+    file = File.open('google-10000-words-between-5-12.txt', 'w')
+    filtered_words.each do |word|
+        file.write(word + "\n")
+    end
+    file.close
+end
+# file = File.open('google-10000-words.txt')
+# google_words = file.read
+# print google_words 
 # Save new lest to a file so we don't have to do that again
 # Check if that file is available and use it
 
