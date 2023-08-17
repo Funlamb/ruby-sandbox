5c383160a6be31cf0cdc9569454250a5f60fbbfd
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-08-16 17:04:31 -0700

    Figured out the core game with letters and saving

--- hangman/game.rb
+++ hangman/game.rb
@@ -1,19 +1,21 @@
+require 'set'
+
 class Game
     attr_reader :word_to_guess, :remaining_guesses, :letters_guessed
 
     def initialize()
         @word_to_guess = get_random_word
         @remaining_guesses = 10
-        @letters_guessed = []
+        @letters_guessed = Set[]
     end
 
     def load_game(game_to_load)
-        # TODO: Load Game
+        # TODO Load Game
         puts "TODO: Load Game"
     end
 
-    def save_game
-        # TODO: Save game
+    def save_game(word_to_guess, remaining_guesses, letters_guessed)
+        # TODO Save game
         puts "Save Game"
     end
     
@@ -31,31 +33,42 @@ class Game
 
     def core_game
         # show the player correct letters
-        while @remaining_guesses > 0
+        still_playing = true
+        while @remaining_guesses > 0 && still_playing
             puts "You have #{remaining_guesses} wrong guesses before you loose the game."
             puts "Your word to guess: " + word_with_blanks(letters_guessed, word_to_guess)
-            puts "What letter would you like to try? "
-            # process letter input
-            letter = get_valid_user_input(letters_guessed)
-
-            @remaining_guesses -= 1
+            puts "What letter would you like to try? (or 'save' to save your game)"
+            is_user_input_valid = false
+            until is_user_input_valid
+                user_input = gets.chomp.downcase
+                if user_input == 'save'
+                    save_game(word_to_guess, remaining_guesses, letters_guessed)
+                    is_user_input_valid = true
+                    still_playing = false
+                elsif user_input.length == 1
+                    if letters_guessed.include?(user_input) # make sure letter is not already used
+                        puts "Error: #{user_input} already used. Please pick again"
+                    else
+                        letters_guessed.push user_input
+                        is_user_input_valid = true
+                    end
+                else
+                    puts "Invalid input. Please chose 1 unused letter or 'save'."
+                end
+                # if the letter exist don't remove a guess
+                unless word_to_guess.include?(user_input)
+                    @remaining_guesses -= 1
+                end
+            end
             puts "\e[H\e[2J"
         end
-            # check if letter is good
-            # show word if letter is good
-            # maybe use gsub with the letters already chosen?
-
-        # show player wrong letters used
-        # clear cmd every input so game stays up top
-
         # win condition
         # lose condition
-
     end
 
+    # Download the 'google-10000-words.txt' text and filter it for words that are 5-12 in length
+    # create a new file with the filtered list so you don't have to filter it again
     def get_random_word
-        # Download the google words
-        # Find words that are 5 to 12 letters long
         unless File.exist?('google-10000-words-between-5-12.txt')
             filtered_words = []
             filtered_words = File.foreach('google-10000-words.txt', chomp: true).select {
@@ -82,18 +95,12 @@ class Game
         return '_ ' * word_to_guess.length 
     end
     
+    # TODO fix save and user letter
     def get_valid_user_input(letters_guessed)
-        letter = gets.chomp
-        if letter == 'save' # save condition
-            return save_the_game(remaining_guesses, word_to_guess, letters_guessed)
-        elsif letters_guessed.include?(letter) # make sure letter is not already used
+        if letters_guessed.include?(letter) # make sure letter is not already used
             puts "Error: #{letter} already used. Please pick again"
-            get_valid_user_input(letters_guessed)
-        elsif letter.length != 1 # make sure only one letter
-            puts "Error: Please guess only one letter"
-            get_valid_user_input(letters_guessed)
         end
-        letters_guessed.push letter
+        @letters_guessed.push letter
         letter
     end
 end
