7b9efcabb7a976a3976325301b5f752266ec3c3b
Michael Kucukarslan <michaelkucukarslan@gmail.com>
2023-08-17 15:26:02 -0700

    Got game to load from a save

--- hangman/game.rb
+++ hangman/game.rb
@@ -1,4 +1,6 @@
 require 'set'
+require 'yaml'
+require 'find'
 
 class Game
     attr_reader :word_to_guess, :remaining_guesses, :letters_guessed
@@ -9,13 +11,45 @@ class Game
         @letters_guessed = Set[]
     end
 
-    def load_game(game_to_load)
+    def load_game()
         # TODO Load Game
-        puts "TODO: Load Game"
+        # get a name of all the files
+        files = []
+        Find.find('.') do |path|
+            files << path if path =~ /.*\.hm$/
+        end
+        # show the files to the user
+        puts files
+        puts "What file would you like to load?"
+        file_to_load = gets.chomp
+        file = File.open("#{file_to_load}.hm")
+        load_yaml(file)
+        core_game
+    end
+
+    def load_yaml(string)
+        data = YAML.load string
+        @word_to_guess = data[:word_to_guess]
+        @remaining_guesses = data[:remaining_guesses]
+        @letters_guessed = data[:letters_guessed]
     end
 
     def save_game(word_to_guess, remaining_guesses, letters_guessed)
         # TODO Save game
+        puts "What do you want to name your save file?"
+        save_file_name = gets.chomp
+        save_file = YAML.dump ({
+            :word_to_guess => @word_to_guess,
+            :remaining_guesses => @remaining_guesses,
+            :letters_guessed => @letters_guessed
+        })
+        begin
+            file = File.open("#{save_file_name}.hm", "w")
+            file.write(save_file)
+            file.close
+        rescue
+            "Failed to save. Please contact your administrator."
+        end
         puts "Save Game"
     end
     
@@ -29,6 +63,10 @@ class Game
 
     def continue_game
         # TODO Continue Game
+        # look for files with .hm
+        # show users names
+        # check for matching name
+        # load file with correct name
     end
 
     def core_game
@@ -60,10 +98,10 @@ class Game
                     @remaining_guesses -= 1
                 end
             end
-            puts "\e[H\e[2J"
+            # puts "\e[H\e[2J"
+            # win condition
+            # lose condition
         end
-        # win condition
-        # lose condition
     end
 
     # Download the 'google-10000-words.txt' text and filter it for words that are 5-12 in length
@@ -93,14 +131,5 @@ class Game
             return word_to_guess.gsub(regexp, '_').chars.join(' ')
         end
         return '_ ' * word_to_guess.length 
-    end
-    
-    # TODO fix save and user letter
-    def get_valid_user_input(letters_guessed)
-        if letters_guessed.include?(letter) # make sure letter is not already used
-            puts "Error: #{letter} already used. Please pick again"
-        end
-        @letters_guessed.push letter
-        letter
     end
 end
